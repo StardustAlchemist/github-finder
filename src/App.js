85bde3dbe7e121a872bc886da8d24d758bd9885c
Cristian Javier Alvarez <cjavi26000@hotmail.com>
2020-11-23 15:32:30 -0600

    Intro To JSX & Fragments

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hello from React</h1>
-      </div>
+    return React.createElement(
+      'div',
+      { className: 'App' },
+      React.createElement('h1', null, 'Hello from React')
     );
   }
 }
